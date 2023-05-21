f568a3b2419dc0f5d8c163be662adcb41f41479e
dell <luckyjeetendra@gmail.com>
2023-05-21 14:09:53 +0530

    Move returnable_product_container to paid modules

--- returnable_product_container/models/product.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-    
-    is_container = fields.Boolean('Is a Container', default=False)
-    
