c11599406da199d2db38a13b5084cf29e79cd390
dell <luckyjeetendra@gmail.com>
2023-05-21 14:18:02 +0530

    Move returnable_product_container to paid modules

--- returnable_product_container/models/account.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
-    
-    is_deposit_line = fields.Boolean(related='product_id.is_container', store=True)
-    
